b538e653aeecc2b5de5f0823a75713c81b229caa
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-08-31 05:10:04 +0300

    Create a class Decorator

---
+++ src/decorator.rb
@@ -0,0 +1,12 @@
+require('./src/nameable')
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
