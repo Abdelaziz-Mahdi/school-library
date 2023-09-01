a50bd650cafdd004dc6fc4daa2341e938065cd1f
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-09-02 02:40:52 +0300

    Create relationship between Classroom and Student

--- src/classroom.rb
+++ src/classroom.rb
@@ -1,10 +1,17 @@
-require ('./src/student')
+require('./src/student')
 
 class Classroom
   attr_accessor :label
+  attr_reader :students
   
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
   end
 
 end
--- src/student.rb
+++ src/student.rb
@@ -1,9 +1,15 @@
 require('./src/person')
+require('./src/classroom')
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)      
   end
 
   def play_hooky
